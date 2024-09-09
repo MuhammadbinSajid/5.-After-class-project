e7b2dccbf01c708b10a62312e54960b7d5809da5
MuhammadbinSajid <mohammedsajidamazai@gmail.com>
2024-09-09 06:21:18 +0500

    initial commit

---
+++ main.py
@@ -0,0 +1,8 @@
+# Input from the user
+age = int(input("Enter your age: "))
+
+# Check if age is between 10 and 20
+if 10 <= age <= 20:
+    print("Your age is between 10 and 20.")
+else:
+    print("Your age is not between 10 and 20.")
